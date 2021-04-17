58a680f1fa76f240d7964fd6d19062ac3ce16975
jamescross <crossjames813@gmail.com>
2021-04-17 11:35:46 -0500

    commit tree 4f922fefbab7831159e213ab409afaf72c7c7552

--- app.rb
+++ app.rb
@@ -2,5 +2,53 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+ get "/reversename/:name" do
+   @user_name = params[:name].reverse
+   "#{@user_name}"
+ end
 
+ get "/square/:number" do
+   @num = params[:number].to_i
+   @square = @num * @num
+   "#{@square}"
+ end
+
+ get "/say/:number/:phrase" do
+  
+   @num = params[:number].to_i
+   @phrase = params[:phrase]
+   
+   @phrase * (@num)
+ end
+
+ get "/say/:word1/:word2/:word3/:word4/:word5" do
+   @word1 = params[:word1]
+   @word2 = params[:word2]
+   @word3 = params[:word3]
+   @word4 = params[:word4]
+   @word5 = params[:word5]
+   "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+ end
+
+ get "/:operation/:number1/:number2" do
+   @operation = params[:operation]
+   @num1 = params[:number1].to_i
+   @num2 = params[:number2].to_i
+  
+   if @operation == "add"
+    @ans = @num1 + @num2
+
+   elsif @operation == "multiply"
+    @ans = @num1 * @num2
+ 
+   elsif @operation == "subtract"
+    @ans = @num1 - @num2
+   
+   else 
+    @ans = @num1/@num2
+  
+   end
+  
+   @ans.to_s
+ end
 end
